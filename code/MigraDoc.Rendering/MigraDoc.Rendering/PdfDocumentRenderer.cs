8651f46f4dd952f5d78440c6b7e2f9a898bd5654
dankennedy <daniel@firstcs.co.uk>
2015-11-24 20:18:22 +0000

    added support for custom properties on document to store additional metadata

--- code/MigraDoc.Rendering/MigraDoc.Rendering/PdfDocumentRenderer.cs
+++ code/MigraDoc.Rendering/MigraDoc.Rendering/PdfDocumentRenderer.cs
@@ -1,4 +1,5 @@
 #region MigraDoc - Creating Documents on the Fly
+
 //
 // Authors:
 //   Klaus Potzesny (mailto:Klaus.Potzesny@pdfsharp.com)
@@ -26,9 +27,11 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 // DEALINGS IN THE SOFTWARE.
+
 #endregion
 
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.IO;
 using MigraDoc.DocumentObjectModel;
@@ -39,128 +42,118 @@ using PdfSharp.Drawing;
 
 namespace MigraDoc.Rendering
 {
-  /// <summary>
-  /// Provides the functionality to convert a MigraDoc document into PDF.
-  /// </summary>
-  public class PdfDocumentRenderer
-  {
     /// <summary>
-    /// Initializes a new instance of the PdfDocumentRenderer class.
+    ///     Provides the functionality to convert a MigraDoc document into PDF.
     /// </summary>
-    public PdfDocumentRenderer()
+    public class PdfDocumentRenderer
     {
-    }
+        /// <summary>
+        ///     Initializes a new instance of the PdfDocumentRenderer class.
+        /// </summary>
+        public PdfDocumentRenderer()
+        {
+        }
 
-    /// <summary>
-    /// Initializes a new instance of the PdfDocumentRenderer class.
-    /// </summary>
-    /// <param name="unicode">If true Unicode encoding is used for all text. If false, WinAnsi encoding is used.</param>
-    public PdfDocumentRenderer(bool unicode)
-    {
-      this.unicode = unicode;
-    }
+        /// <summary>
+        ///     Initializes a new instance of the PdfDocumentRenderer class.
+        /// </summary>
+        /// <param name="unicode">If true Unicode encoding is used for all text. If false, WinAnsi encoding is used.</param>
+        public PdfDocumentRenderer(bool unicode)
+        {
+            Unicode = unicode;
+        }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="PdfDocumentRenderer"/> class.
-    /// </summary>
-    /// <param name="unicode">If true Unicode encoding is used for all text. If false, WinAnsi encoding is used.</param>
-    /// <param name="fontEmbedding">Specifies which fonts will be imbedded in the PDF file.</param>
-    public PdfDocumentRenderer(bool unicode, PdfFontEmbedding fontEmbedding)
-    {
-      this.unicode = unicode;
-      this.fontEmbedding = fontEmbedding;
-    }
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PdfDocumentRenderer" /> class.
+        /// </summary>
+        /// <param name="unicode">If true Unicode encoding is used for all text. If false, WinAnsi encoding is used.</param>
+        /// <param name="fontEmbedding">Specifies which fonts will be imbedded in the PDF file.</param>
+        public PdfDocumentRenderer(bool unicode, PdfFontEmbedding fontEmbedding)
+        {
+            Unicode = unicode;
+            FontEmbedding = fontEmbedding;
+        }
 
-    /// <summary>
-    /// Gets a value indicating whether the text is rendered as Unicode.
-    /// </summary>
-    public bool Unicode
-    {
-      get { return this.unicode; }
-    }
-    bool unicode;
+        /// <summary>
+        ///     Gets a value indicating whether the text is rendered as Unicode.
+        /// </summary>
+        public bool Unicode { get; }
 
-    /// <summary>
-    /// Gets a value indicating whether all used fonts are embedded in the document.
-    /// </summary>
-    public PdfFontEmbedding FontEmbedding
-    {
-      get { return this.fontEmbedding; }
-    }
-    PdfFontEmbedding fontEmbedding;
+        /// <summary>
+        ///     Gets a value indicating whether all used fonts are embedded in the document.
+        /// </summary>
+        public PdfFontEmbedding FontEmbedding { get; }
 
-    /// <summary>
-    /// Gets or sets the language.
-    /// </summary>
-    /// <value>The language.</value>
-    public string Language
-    {
-      get { return this.language; }
-      set { this.language = value; }
-    }
-    string language = String.Empty;
+        /// <summary>
+        ///     Gets or sets the language.
+        /// </summary>
+        /// <value>The language.</value>
+        public string Language { get; set; } = string.Empty;
 
-    /// <summary>
-    /// Set the MigraDoc document to be rendered by this printer.
-    /// </summary>
-    public Document Document
-    {
-      set
-      {
-        this.document = null;
-        value.BindToRenderer(this);
-        this.document = value;
-      }
-    }
-    Document document;
+        /// <summary>
+        ///     Set the MigraDoc document to be rendered by this printer.
+        /// </summary>
+        public Document Document
+        {
+            set
+            {
+                document = null;
+                value.BindToRenderer(this);
+                document = value;
+            }
+        }
 
-    /// <summary>
-    /// Gets or sets a document renderer.
-    /// </summary>
-    /// <remarks>
-    /// A document renderer is automatically created and prepared
-    /// when printing before this property was set.
-    /// </remarks>
-    public DocumentRenderer DocumentRenderer
-    {
-      get
-      {
-        if (this.documentRenderer == null)
-          PrepareDocumentRenderer();
-        return this.documentRenderer;
-      }
-      set { this.documentRenderer = value; }
-    }
-    DocumentRenderer documentRenderer;
+        Document document;
 
-    void PrepareDocumentRenderer()
-    {
-      PrepareDocumentRenderer(false);
-    }
+        /// <summary>
+        ///     Gets or sets a document renderer.
+        /// </summary>
+        /// <remarks>
+        ///     A document renderer is automatically created and prepared
+        ///     when printing before this property was set.
+        /// </remarks>
+        public DocumentRenderer DocumentRenderer
+        {
+            get
+            {
+                if (documentRenderer == null)
+                    PrepareDocumentRenderer();
+                return documentRenderer;
+            }
+            set { documentRenderer = value; }
+        }
 
-    void PrepareDocumentRenderer(bool prepareCompletely)
-    {
-      if (this.document == null)
-        throw new InvalidOperationException(Messages.PropertyNotSetBefore("DocumentRenderer", MethodInfo.GetCurrentMethod().Name));
+        DocumentRenderer documentRenderer;
 
-      if (this.documentRenderer == null)
-      {
-        this.documentRenderer = new DocumentRenderer(this.document);
-        this.documentRenderer.WorkingDirectory = this.workingDirectory;
-      }
-      if (prepareCompletely && this.documentRenderer.formattedDocument == null)
-      {
-        this.documentRenderer.PrepareDocument();
-      }
-    }
+        void PrepareDocumentRenderer()
+        {
+            PrepareDocumentRenderer(false);
+        }
 
-    /// <summary>
-    /// Renders the document into a PdfDocument containing all pages of the document.
-    /// </summary>
-    public void RenderDocument()
-    {
+        void PrepareDocumentRenderer(bool prepareCompletely)
+        {
+            if (document == null)
+                throw new InvalidOperationException(Messages.PropertyNotSetBefore("DocumentRenderer",
+                    MethodBase.GetCurrentMethod().Name));
+
+            if (documentRenderer == null)
+            {
+                documentRenderer = new DocumentRenderer(document);
+                documentRenderer.WorkingDirectory = WorkingDirectory;
+            }
+            if (prepareCompletely && documentRenderer.formattedDocument == null)
+            {
+                documentRenderer.PrepareDocument();
+            }
+        }
+
+        /// <summary>
+        ///     Renders the document into a PdfDocument containing all pages of the document.
+        /// </summary>
+        public void RenderDocument()
+        {
 #if true
-      PrepareRenderPages();
+            PrepareRenderPages();
 #else
       if (this.documentRenderer == null)
         PrepareDocumentRenderer();
@@ -173,153 +166,158 @@ namespace MigraDoc.Rendering
 
       WriteDocumentInformation();
 #endif
-      RenderPages(1, this.documentRenderer.FormattedDocument.PageCount);
-    }
-
-    /// <summary>
-    /// Renders the document into a PdfDocument containing all pages of the document.
-    /// </summary>
-    public void PrepareRenderPages()
-    {
-      //if (this.documentRenderer == null)
-      PrepareDocumentRenderer(true);
-
-      if (this.pdfDocument == null)
-      {
-        this.pdfDocument = CreatePdfDocument();
-        if (this.document.UseCmykColor)
-          this.pdfDocument.Options.ColorMode = PdfColorMode.Cmyk;
-      }
-
-      WriteDocumentInformation();
-      //RenderPages(1, this.documentRenderer.FormattedDocument.PageCount);
-    }
-
-    /// <summary>
-    /// Gets the count of pages.
-    /// </summary>
-    public int PageCount
-    {
-      get { return this.documentRenderer.FormattedDocument.PageCount; }
-    }
-
-    /// <summary>
-    /// Saves the PdfDocument to the specified path. If a file already exists, it will be overwritten.
-    /// </summary>
-    public void Save(string path)
-    {
-      if (path == null)
-        throw new ArgumentNullException("path");
-
-      else if (path == "")
-        throw new ArgumentException("PDF file Path must not be empty");
-
-      if (this.workingDirectory != null)
-        Path.Combine(this.workingDirectory, path);
-
-      this.pdfDocument.Save(path);
-    }
-
-    /// <summary>
-    /// Saves the PDF document to the specified stream.
-    /// </summary>
-    public void Save(Stream stream, bool closeStream)
-    {
-      this.pdfDocument.Save(stream, closeStream);
-    }
-
-    /// <summary>
-    /// Renders the spcified page range.
-    /// </summary>
-    /// <param name="startPage">The first page to print.</param>
-    /// <param name="endPage">The last page to print</param>
-    public void RenderPages(int startPage, int endPage)
-    {
-      if (startPage < 1)
-        throw new ArgumentOutOfRangeException("startPage");
-
-      if (endPage > this.documentRenderer.FormattedDocument.PageCount)
-        throw new ArgumentOutOfRangeException("endPage");
-
-      if (this.documentRenderer == null)
-        PrepareDocumentRenderer();
-
-      if (this.pdfDocument == null)
-        this.pdfDocument = CreatePdfDocument();
-
-      this.documentRenderer.printDate = DateTime.Now;
-      for (int pageNr = startPage; pageNr <= endPage; ++pageNr)
-      {
-        PdfPage pdfPage = this.pdfDocument.AddPage();
-        PageInfo pageInfo = this.documentRenderer.FormattedDocument.GetPageInfo(pageNr);
-        pdfPage.Width = pageInfo.Width;
-        pdfPage.Height = pageInfo.Height;
-        pdfPage.Orientation = pageInfo.Orientation;
-
-        using (XGraphics gfx = XGraphics.FromPdfPage(pdfPage))
-        {
-          gfx.MUH = this.unicode ? PdfFontEncoding.Unicode : PdfFontEncoding.WinAnsi;
-          gfx.MFEH = this.fontEmbedding;
-          this.documentRenderer.RenderPage(gfx, pageNr);
+            RenderPages(1, documentRenderer.FormattedDocument.PageCount);
         }
-      }
+
+        /// <summary>
+        ///     Renders the document into a PdfDocument containing all pages of the document.
+        /// </summary>
+        public void PrepareRenderPages()
+        {
+            //if (this.documentRenderer == null)
+            PrepareDocumentRenderer(true);
+
+            if (PdfDocument == null)
+            {
+                PdfDocument = CreatePdfDocument();
+                if (document.UseCmykColor)
+                    PdfDocument.Options.ColorMode = PdfColorMode.Cmyk;
+            }
+
+            WriteDocumentInformation();
+            //RenderPages(1, this.documentRenderer.FormattedDocument.PageCount);
+        }
+
+        /// <summary>
+        ///     Gets the count of pages.
+        /// </summary>
+        public int PageCount
+        {
+            get { return documentRenderer.FormattedDocument.PageCount; }
+        }
+
+        /// <summary>
+        ///     Saves the PdfDocument to the specified path. If a file already exists, it will be overwritten.
+        /// </summary>
+        public void Save(string path)
+        {
+            if (path == null)
+                throw new ArgumentNullException("path");
+
+            if (path == "")
+                throw new ArgumentException("PDF file Path must not be empty");
+
+            if (WorkingDirectory != null)
+                Path.Combine(WorkingDirectory, path);
+
+            PdfDocument.Save(path);
+        }
+
+        /// <summary>
+        ///     Saves the PDF document to the specified stream.
+        /// </summary>
+        public void Save(Stream stream, bool closeStream)
+        {
+            PdfDocument.Save(stream, closeStream);
+        }
+
+        /// <summary>
+        ///     Renders the spcified page range.
+        /// </summary>
+        /// <param name="startPage">The first page to print.</param>
+        /// <param name="endPage">The last page to print</param>
+        public void RenderPages(int startPage, int endPage)
+        {
+            if (startPage < 1)
+                throw new ArgumentOutOfRangeException("startPage");
+
+            if (endPage > documentRenderer.FormattedDocument.PageCount)
+                throw new ArgumentOutOfRangeException("endPage");
+
+            if (documentRenderer == null)
+                PrepareDocumentRenderer();
+
+            if (PdfDocument == null)
+                PdfDocument = CreatePdfDocument();
+
+            documentRenderer.printDate = DateTime.Now;
+            for (var pageNr = startPage; pageNr <= endPage; ++pageNr)
+            {
+                var pdfPage = PdfDocument.AddPage();
+                var pageInfo = documentRenderer.FormattedDocument.GetPageInfo(pageNr);
+                pdfPage.Width = pageInfo.Width;
+                pdfPage.Height = pageInfo.Height;
+                pdfPage.Orientation = pageInfo.Orientation;
+
+                using (var gfx = XGraphics.FromPdfPage(pdfPage))
+                {
+                    gfx.MUH = Unicode ? PdfFontEncoding.Unicode : PdfFontEncoding.WinAnsi;
+                    gfx.MFEH = FontEmbedding;
+                    documentRenderer.RenderPage(gfx, pageNr);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Gets or sets a working directory for the printing process.
+        /// </summary>
+        public string WorkingDirectory { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the PDF document to render on.
+        /// </summary>
+        /// <remarks>A PDF document in memory is automatically created when printing before this property was set.</remarks>
+        public PdfDocument PdfDocument { get; set; }
+
+        /// <summary>
+        ///     Writes document information like author and subject to the PDF document.
+        /// </summary>
+        public void WriteDocumentInformation()
+        {
+            if (!document.IsNull("Info"))
+            {
+                var docInfo = document.Info;
+                var pdfInfo = PdfDocument.Info;
+
+                if (!docInfo.IsNull("Author"))
+                    pdfInfo.Author = docInfo.Author;
+
+                if (!docInfo.IsNull("Keywords"))
+                    pdfInfo.Keywords = docInfo.Keywords;
+
+                if (!docInfo.IsNull("Subject"))
+                    pdfInfo.Subject = docInfo.Subject;
+
+                if (!docInfo.IsNull("Title"))
+                    pdfInfo.Title = docInfo.Title;
+
+                if (CustomProperties != null)
+                {
+                    foreach (var prop in CustomProperties)
+                    {
+                        pdfInfo.Elements.Add(!prop.Key.StartsWith("/")
+                            ? new KeyValuePair<string, PdfItem>("/" + prop.Key, prop.Value)
+                            : prop);
+                    }
+                }
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public IEnumerable<KeyValuePair<string, PdfItem>> CustomProperties { get; set; }        
+
+        /// <summary>
+        ///     Creates a new PDF document.
+        /// </summary>
+        PdfDocument CreatePdfDocument()
+        {
+            var document = new PdfDocument();
+            document.Info.Creator = VersionInfo.Creator;
+            if (Language != null && Language.Length != 0)
+                document.Language = Language;
+            return document;
+        }
     }
-
-    /// <summary>
-    /// Gets or sets a working directory for the printing process.
-    /// </summary>
-    public string WorkingDirectory
-    {
-      get { return this.workingDirectory; }
-      set { this.workingDirectory = value; }
-    }
-    string workingDirectory;
-
-    /// <summary>
-    /// Gets or sets the PDF document to render on.
-    /// </summary>
-    /// <remarks>A PDF document in memory is automatically created when printing before this property was set.</remarks>
-    public PdfDocument PdfDocument
-    {
-      get { return this.pdfDocument; }
-      set { this.pdfDocument = value; }
-    }
-    PdfDocument pdfDocument;
-
-    /// <summary>
-    /// Writes document information like author and subject to the PDF document.
-    /// </summary>
-    public void WriteDocumentInformation()
-    {
-      if (!this.document.IsNull("Info"))
-      {
-        DocumentInfo docInfo = this.document.Info;
-        PdfDocumentInformation pdfInfo = this.pdfDocument.Info;
-
-        if (!docInfo.IsNull("Author"))
-          pdfInfo.Author = docInfo.Author;
-
-        if (!docInfo.IsNull("Keywords"))
-          pdfInfo.Keywords = docInfo.Keywords;
-
-        if (!docInfo.IsNull("Subject"))
-          pdfInfo.Subject = docInfo.Subject;
-
-        if (!docInfo.IsNull("Title"))
-          pdfInfo.Title = docInfo.Title;
-      }
-    }
-
-    /// <summary>
-    /// Creates a new PDF document.
-    /// </summary>
-    PdfDocument CreatePdfDocument()
-    {
-      PdfDocument document = new PdfDocument();
-      document.Info.Creator = VersionInfo.Creator;
-      if (this.language != null && this.language.Length != 0)
-        document.Language = this.language;
-      return document;
-    }
-  }
 }
